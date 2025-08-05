be71769f189262816595f9114ae401beb4b9f1bc
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-05 22:48:13 +0000

    Update dependencies

--- frontend/src/telemetry/setup.js
+++ frontend/src/telemetry/setup.js
@@ -11,12 +11,16 @@ import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
 import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
-import { trace } from '@opentelemetry/api';
+import { context, trace } from '@opentelemetry/api';
 
 
 export const setupTelemetry = () => {
   try {
-    const provider = new WebTracerProvider();
+    const resource = new Resource(attributes={
+        SERVICE_NAME: "elasticsearch-ai-frontend"
+    });
+
+    const provider = new WebTracerProvider({ resource });
     
     // Configure OTLP exporter
     const otlpExporter = new OTLPTraceExporter({
