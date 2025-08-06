9e57711af7636b81485e9dd399b75625cb5e9a9c
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-06 03:43:23 +0000

    Update depdency

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,7 +3,7 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
         url: require.resolve('url'),
         assert: require.resolve('assert'),
-        crypto: require.resolve('crypto-browserify'),
+        crypto: false,
         http: require.resolve('stream-http'),
         https: require.resolve('https-browserify'),
         os: require.resolve('os-browserify/browser'),
