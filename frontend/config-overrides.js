d237ed9a848d2ea19be108f2c2a03b0adf186d74
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-06 13:33:26 +0000

    trace

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,37 +3,37 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        child_process: false,
-        fs: false,
-        path: false,
-        net: false,
-        tls: false,
-        dns: false,
-        http2: false,
-        os: false,
-        zlib: false,
-        events: false,
-        timers: require.resolve('timers-browserify'),
-        console: false,
-        domain: false,
-        async_hooks: false,
-        diagnostics_channel: false,
-        crypto: false,
-        querystring: require.resolve("querystring-es3"),
-        url: require.resolve('url/'),
-        assert: require.resolve('assert'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        buffer: require.resolve('buffer'),
-        stream: require.resolve('stream-browserify'),
-    };
-    config.plugins.push(
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    );
+    // config.resolve.fallback = {
+    //     child_process: false,
+    //     fs: false,
+    //     path: false,
+    //     net: false,
+    //     tls: false,
+    //     dns: false,
+    //     http2: false,
+    //     os: false,
+    //     zlib: false,
+    //     events: false,
+    //     timers: require.resolve('timers-browserify'),
+    //     console: false,
+    //     domain: false,
+    //     async_hooks: false,
+    //     diagnostics_channel: false,
+    //     crypto: false,
+    //     querystring: require.resolve("querystring-es3"),
+    //     url: require.resolve('url/'),
+    //     assert: require.resolve('assert'),
+    //     http: require.resolve('stream-http'),
+    //     https: require.resolve('https-browserify'),
+    //     buffer: require.resolve('buffer'),
+    //     stream: require.resolve('stream-browserify'),
+    // };
+    // config.plugins.push(
+    //     new webpack.ProvidePlugin({
+    //         process: 'process/browser',
+    //         Buffer: ['buffer', 'Buffer'],
+    //     }),
+    // );
 
     // Add cache configuration
     config.cache = {
