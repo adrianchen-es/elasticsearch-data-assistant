addf7721d0601eca1a65d0b5de50cabbb5fbbbbd
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-06 03:58:14 +0000

    Update dependency versions in the package.json file

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -3,7 +3,7 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
         url: require.resolve('url/'),
         assert: require.resolve('assert'),
-        crypto: false,
+        crypto: require.resolve('crypto-browserify'),
         http: require.resolve('stream-http'),
         https: require.resolve('https-browserify'),
         os: require.resolve('os-browserify/browser'),
