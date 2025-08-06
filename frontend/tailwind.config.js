6898ea196351b70d96f2a37a188934b30be0b965
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-06 03:36:37 +0000

    update build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,16 +7,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  resolve: {
-    fallback: {
-      "buffer": require.resolve("buffer/"),
-      "stream": require.resolve("stream-browserify"),
-      "crypto": require.resolve("crypto-browserify"),
-      "assert": require.resolve("assert/"),
-      "util": require.resolve("util/"),
-      "process": require.resolve("process/browser"),
-      "path": require.resolve("path-browserify"),
-      "http": require.resolve("stream-http"),
-    }
-  },
 }
