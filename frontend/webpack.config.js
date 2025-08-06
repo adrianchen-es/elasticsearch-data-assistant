c67e745788415815151687829efa9247b20182c7
adrianchen-es <95603111+adrianchen-es@users.noreply.github.com>
2025-08-06 03:28:24 +0000

    Include webpack config

---
+++ frontend/webpack.config.js
@@ -0,0 +1,22 @@
+// filepath: /workspaces/elasticsearch-data-assistant/frontend/webpack.config.js
+const webpack = require('webpack');
+
+module.exports = {
+  // ...existing config...
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "assert": require.resolve("assert/"),
+      "util": require.resolve("util/"),
+      "process": require.resolve("process/browser"),
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+  ],
+};
